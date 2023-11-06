3459aa2f8e62251e2e73d5301cd149197eaef96b
Krzysztof Kochanowski <Krzysztof.Kochanowski@ttms.pl>
2023-11-06 21:15:19 +0100

    Design patterns/builder

---
+++ KK.DesignPattern.Builder/Builders/CarBuilderElectric.cs
@@ -0,0 +1,36 @@
+﻿namespace KK.DesignPattern.Builder.Builders
+{
+    internal class CarBuilderElectric : ICarBuilder
+    {
+        private readonly Car _car = new Car();
+
+        public ICarBuilder SetSeats(string value)
+        {
+            this._car.Seats = $"ElectricCar - {value}";
+            return this;
+        }
+
+        public ICarBuilder SetEngine(string value)
+        {
+            this._car.Engine = $"ElectricCar - {value}";
+            return this;
+        }
+
+        public ICarBuilder SetGps(string value)
+        {
+            this._car.Gps = $"ElectricCar - {value}";
+            return this;
+        }
+
+        public ICarBuilder SetSunroof(string value)
+        {
+            this._car.Sunroof = $"ElectricCar - {value}";
+            return this;
+        }
+
+        public Car Build()
+        {
+            return _car;
+        }
+    }
+}
--- README.md
+++ README.md
@@ -1,1 +1,4 @@
-# DesignPatterns
+# About Repository
+
+This repository contains definitions, diagrams and examples of using common Design Patterns. Examples are mainly written in C#. 
+Each pattern has some links to usefull documentation or video tutorial. :smile: 

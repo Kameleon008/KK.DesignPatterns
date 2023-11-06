e950d80ffdf56f4faf245bb0dfb97d95c9eb71db
Krzysztof Kochanowski <Krzysztof.Kochanowski@ttms.pl>
2023-11-06 21:15:19 +0100

    Design patterns/builder (#2)

---
+++ KK.DesignPattern.Builder/CarDealer.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using KK.DesignPattern.Builder.Builders;
+
+namespace KK.DesignPattern.Builder
+{
+    internal class CarDealer
+    {
+        private CarManufacturer manufacturer = new CarManufacturer();
+
+        private CarBuilderSport sportCarBuilder = new CarBuilderSport();
+
+        private CarBuilderElectric electricCarBuilder = new CarBuilderElectric();
+
+        public Car OrderSportCar()
+        {
+            this.manufacturer.SetBuilder(sportCarBuilder);
+            return this.manufacturer.Construct();
+        }
+
+        public Car OrderElectricCar()
+        {
+            this.manufacturer.SetBuilder(electricCarBuilder);
+            return this.manufacturer.Construct();
+        }
+    }
+}
---
+++ KK.DesignPattern.Builder/Exceptions/CarBuilderNotSetException.cs
@@ -0,0 +1,20 @@
+﻿namespace KK.DesignPattern.Builder.Exceptions
+{
+    internal class CarBuilderNotSetException : Exception
+    {
+        public CarBuilderNotSetException()
+            : base("Car Builder not set.")
+        {
+        }
+
+        public CarBuilderNotSetException(string message)
+            : base(message)
+        {
+        }
+
+        public CarBuilderNotSetException(string message, Exception innerException)
+            : base(message, innerException)
+        {
+        }
+    }
+}

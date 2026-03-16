22da201128e0684ab2043684c43522542112743d
Krzysztof Kochanowski <krzysztofkochanowski96@gmail.com>
2026-03-16 13:00:31 +0100

    Add Abstract Factory pattern documentation and update related code files

---
+++ KK.DesignPattern.AbstractFactory/Components/Buttons/MacButton.cs
@@ -0,0 +1,17 @@
+﻿namespace KK.DesignPattern.AbstractFactory.Components.Buttons
+{
+    internal class MacButton(string name) : IButton
+    {
+        public string Name { get; } = name;
+
+        public void Render()
+        {
+            Console.WriteLine("MacButton: [{0}]", this.Name);
+        }
+
+        public void Press()
+        {
+            Console.WriteLine("[{0}] Pressed on MAC!", this.Name);
+        }
+    }
+}

e09986283d9b974f9886dc722bffa1042c433056
Krzysztof Kochanowski <krzysztofkochanowski96@gmail.com>
2026-03-17 13:18:36 +0100

    Create Decorator implementation example

---
+++ KK.DesignPattern.Decorator/Decorators/SmsNotifier.cs
@@ -0,0 +1,11 @@
+﻿namespace KK.DesignPattern.Decorator.Decorators
+{
+    internal class SmsNotifier(INotifier notifier) : BaseDecorator(notifier)
+    {
+        public override void Notify(string message)
+        {
+            base.Notify(message);
+            Console.WriteLine("Send Notification SMS: {0}", message);
+        }
+    }
+}
--- KK.DesignPattern.Decorator/Program.cs
+++ KK.DesignPattern.Decorator/Program.cs
@@ -1,2 +1,28 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using KK.DesignPattern.Decorator;
+using KK.DesignPattern.Decorator.Decorators;
+
+public static class Program
+{
+    public static void Main()
+    {
+        Console.WriteLine();
+        var notify = new Notifier();
+        notify.Notify("Message");
+
+        Console.WriteLine();
+        var smsNotify = new SmsNotifier(notify);
+        smsNotify.Notify("Notify wrapped with sms");
+
+        Console.WriteLine();
+        var emailNotify = new EmailNotifier(notify);
+        emailNotify.Notify("Notify wrapped with email");
+
+        Console.WriteLine();
+        var smsAndEmailNotify = new EmailNotifier(smsNotify);
+        smsAndEmailNotify.Notify("Notify wrapped with sms and email");
+
+        Console.WriteLine();
+        var emailAndSmsNotify = new SmsNotifier(emailNotify);
+        emailAndSmsNotify.Notify("Notify wrapped with email and sms");
+    }
+}

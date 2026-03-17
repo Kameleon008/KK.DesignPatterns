56179fb8261835652474a1440d4db6ee6b37941b
Krzysztof Kochanowski <59693621+Kameleon008@users.noreply.github.com>
2026-03-18 00:41:04 +0100

    Design patterns/memento (#17)

---
+++ KK.DesignPattern.Memento/Program.cs
@@ -0,0 +1,23 @@
+﻿namespace KK.DesignPattern.Memento;
+
+public static class Program
+{
+    public static void Main()
+    {
+        var editor = new TextEditor();
+        var history = new History();
+        
+        editor.Content = "First version";
+        history.Push(editor.Save());
+
+        editor.Content = "Second Version (some mistakes where made)";
+
+        Console.WriteLine($"Actual content: {editor.Content}");
+
+        // Revert to firs version
+        var previousState = history.Pop();
+        editor.Restore(previousState);
+
+        Console.WriteLine($"Content after restore: {editor.Content}");
+    }
+}
---
+++ KK.DesignPattern.Memento/TextEditor.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace KK.DesignPattern.Memento
+{
+    internal class TextEditor
+    {
+        public string Content { get; set; }
+
+        public TextEditorMemento Save()
+        {
+            Console.WriteLine("Saving State...");
+            return new TextEditorMemento(this.Content);
+        }
+
+        public void Restore(TextEditorMemento memento)
+        {
+            this.Content = memento.Content;
+            Console.WriteLine($"State from {memento.CreatedAt} restored");
+        }
+    }
+}

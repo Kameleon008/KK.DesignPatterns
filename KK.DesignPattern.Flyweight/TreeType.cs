9eadc181d428d919dcb8c6e469dad2bf049b40fc
Krzysztof Kochanowski <krzysztofkochanowski96@gmail.com>
2026-03-17 14:10:33 +0100

    Create Flyweight implementation example

---
+++ KK.DesignPattern.Flyweight/TreeType.cs
@@ -0,0 +1,16 @@
+﻿namespace KK.DesignPattern.Flyweight
+{
+    internal class TreeType(string name, string color, string texture)
+    {
+        public string Name { get; } = name;
+
+        public string Color { get; } = color;
+
+        public string Texture { get; } = texture;
+
+        public void Draw(int x, int y)
+        {
+            Console.WriteLine($"Draw tree {Name} (Color: {Color}, Texture: {Texture}) on position [{x}, {y}]");
+        }
+    }
+}

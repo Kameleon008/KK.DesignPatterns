afeb89aef49fe8d56fc902e75e291a8ada8913c8
Krzysztof Kochanowski <krzysztofkochanowski96@gmail.com>
2026-03-17 22:31:49 +0100

    Create Observer implementation example

---
+++ KK.DesignPattern.Observer/EventListeners/EventListenerLogging.cs
@@ -0,0 +1,10 @@
+﻿namespace KK.DesignPattern.Observer.EventListeners
+{
+    internal class EventListenerLogging : IEventListener
+    {
+        public void Update(string eventContent)
+        {
+            Console.WriteLine($"LOGGING: {eventContent}");
+        }
+    }
+}
---
+++ KK.DesignPattern.Observer/EventListeners/IEventListener.cs
@@ -0,0 +1,7 @@
+﻿namespace KK.DesignPattern.Observer.EventListeners
+{
+    internal interface IEventListener
+    {
+        public void Update(string eventContent);
+    }
+}

b77c8f5043d0d0b7761b002600112de1cd37400b
Krzysztof Kochanowski <krzysztofkochanowski96@gmail.com>
2026-03-17 21:32:25 +0100

    Create Iterator implementation example

---
+++ KK.DesignPattern.Iterator/Iterators/IteratorAggregate.cs
@@ -0,0 +1,9 @@
+﻿using System.Collections;
+
+namespace KK.DesignPattern.Iterator.Iterators
+{
+    internal abstract class IteratorAggregate : IEnumerable
+    {
+        public abstract IEnumerator GetEnumerator();
+    }
+}

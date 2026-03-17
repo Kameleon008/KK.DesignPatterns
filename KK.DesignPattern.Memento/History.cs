3b8bcda45001e3331dee7f13a69dd09470be6c43
Krzysztof Kochanowski <krzysztofkochanowski96@gmail.com>
2026-03-17 21:51:15 +0100

    Create Memento implementation example

---
+++ KK.DesignPattern.Memento/History.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace KK.DesignPattern.Memento
+{
+    internal class History
+    {
+        private readonly Stack<TextEditorMemento> _history = new Stack<TextEditorMemento>();
+
+        public void Push(TextEditorMemento memento) => _history.Push(memento);
+
+        public TextEditorMemento Pop() => _history.Pop();
+    }
+}
--- KK.DesignPattern.Memento/Program.cs
+++ KK.DesignPattern.Memento/Program.cs
@@ -1,2 +1,23 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿namespace KK.DesignPattern.Memento;
+
+public static class Program
+{
+    public static void Main()
+    {
+        var editor = new TextEditor();
+        var history = new History();
+        
+        editor.Content = "First version";
+        history.Push(editor.Save());
+
+        editor.Content = "Second Version (some mistakes where made)";
+
+        Console.WriteLine($"Actual content: {editor.Content}");
+
+        // Revert to firs version
+        var previousState = history.Pop();
+        editor.Restore(previousState);
+
+        Console.WriteLine($"Content after restore: {editor.Content}");
+    }
+}
